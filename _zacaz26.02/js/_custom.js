6ff70114caec1c5369810b22c512e77a08497fb2
Горбачев Юрий <mefaz.ru@gmail.com>
2020-02-26 16:43:11 +0200

    1

---
+++ _zacaz26.02/js/_custom.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:3,
+    nav:false,
+    items:3,
+    center:true
+})
+
+});
