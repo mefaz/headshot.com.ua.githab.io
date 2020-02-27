798e9213aef7439eb4d47ba3be2d89be0b3dcea6
Горбачев Юрий <mefaz.ru@gmail.com>
2020-02-27 10:41:29 +0200

    3

--- zacaz2602/js/_custom.js
+++ zacaz2602/js/_custom.js
@@ -7,5 +7,27 @@ $('.owl-carousel').owlCarousel({
     items:3,
     center:true
 })
+var innerWidthActiv = $('.box-2 .owl-carousel .owl-item.center .item').innerWidth(),
+		innerWidth = $('.box-2 .owl-carousel .owl-item .item').innerWidth();
+		$('.box-2 .owl-carousel .owl-item .item').innerHeight(innerWidth - 25);
+		$('.box-2 .owl-carousel .owl-item.center .item').innerHeight(innerWidthActiv);
+		$('.owl-carousel').innerHeight(innerWidthActiv);
+
+		$('.box-2 .owl-carousel .item .box-img img').innerWidth(innerWidth / 3);
+
+$('.owl-carousel').on('dragged.owl.carousel', function(event) {
+		var innerWidthActiv = $('.box-2 .owl-carousel .owl-item.center .item').innerWidth(),
+				innerWidth = $('.box-2 .owl-carousel .owl-item .item').innerWidth();
+		console.log(innerWidthActiv);
+		console.log(innerWidth);
+		$('.box-2 .owl-carousel .owl-item .item').innerHeight(innerWidth-25);
+		$('.box-2 .owl-carousel .owl-item.active.center .item').innerHeight(innerWidthActiv);
+})
+
+var itemWidthActiv = $('.box-2 .box-item .item').innerWidth();
+		$('.box-2 .box-item .item').innerHeight(itemWidthActiv);
+		$('.box-2 .box-item .item .box-img').innerWidth(itemWidthActiv - 50);
+
+
 
 });
